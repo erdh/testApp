97fc44a3aea0831931f2848700d7206a63624055
erdh <47626170+erdh@users.noreply.github.com>
2021-03-24 22:31:45 +0300

    Fixed a typo

--- app/src/main/java/com/example/test/ui/Profile/SlideshowViewModel.kt
+++ app/src/main/java/com/example/test/ui/Profile/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
+        value = "This is profile page"
     }
     val text: LiveData<String> = _text
 }
