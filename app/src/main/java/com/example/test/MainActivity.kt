6c63c2516ef73845b07f46543812436dcd507642
erdh <47626170+erdh@users.noreply.github.com>
2021-03-24 21:55:43 +0300

    Added "Login" to app drawer

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.loginActivity, R.id.nav_home, R.id.nav_ranking, R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
