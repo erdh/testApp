a4bed65690c6490718f44b8f5f3e4d8a7ae699b0
erdh <47626170+erdh@users.noreply.github.com>
2021-04-26 16:31:41 +0300

    added strings and we have a great welcome toast

--- app/src/main/java/com/example/test/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/test/ui/login/LoginActivity.kt
@@ -102,17 +102,21 @@ class LoginActivity : AppCompatActivity() {
             }
 
             login.setOnClickListener {
+                val welcome = getString(R.string.welcome)
                 auth.signInWithEmailAndPassword(username.text.toString(), password.text.toString())
                 .addOnCompleteListener() { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(baseContext, welcome + " " + username.text,
+                            Toast.LENGTH_SHORT).show()
 
                         val user = auth.currentUser
                         updateUI(user)
                     } else {
+                        val login_failed = getString(R.string.login_failed)
                         // If sign in fails, display a message to the user.
                         Log.w( "signInWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.makeText(baseContext, login_failed,
                             Toast.LENGTH_SHORT).show()
                         updateUI(null)
                     }
