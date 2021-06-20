67931c28ff33cb6329a9986b03df66af3861cd25
erdh <47626170+erdh@users.noreply.github.com>
2021-06-20 13:36:03 +0300

    maindestination düzeltilmesi lazım, başka bir şey yok

---
+++ app/src/main/java/com/example/test/utils/NetworkImage.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.test.ui.utils
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
+import coil.intercept.Interceptor
+import coil.request.ImageResult
+import coil.size.PixelSize
+import com.example.test.R
+import com.example.test.ui.theme.compositedOnSurface
+import com.google.accompanist.coil.LocalImageLoader
+import com.google.accompanist.coil.rememberCoilPainter
+import com.google.accompanist.imageloading.ImageLoadState
+import okhttp3.HttpUrl
+
+/**
+ * A wrapper around [Image] and [rememberCoilPainter], setting a
+ * default [contentScale] and showing content while loading.
+ */
+@Composable
+fun NetworkImage(
+    url: String,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    placeholderColor: Color? = MaterialTheme.colors.compositedOnSurface(0.2f)
+) {
+    Box(modifier) {
+        val painter = rememberCoilPainter(
+            request = url,
+            previewPlaceholder = R.drawable.photo_architecture,
+        )
+
+        Image(
+            painter = painter,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        if (painter.loadState is ImageLoadState.Loading && placeholderColor != null) {
+            Spacer(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(placeholderColor)
+            )
+        }
+    }
+}
+
+@Composable
+fun ProvideImageLoader(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    val loader = remember(context) {
+        ImageLoader.Builder(context)
+            .componentRegistry {
+                add(UnsplashSizingInterceptor)
+            }.build()
+    }
+    CompositionLocalProvider(LocalImageLoader provides loader, content = content)
+}
+
+/**
+ * A Coil [Interceptor] which appends query params to Unsplash urls to request sized images.
+ */
+@OptIn(ExperimentalCoilApi::class)
+object UnsplashSizingInterceptor : Interceptor {
+    override suspend fun intercept(chain: Interceptor.Chain): ImageResult {
+        val data = chain.request.data
+        val size = chain.size
+        if (data is String &&
+            data.startsWith("https://images.unsplash.com/photo-") &&
+            size is PixelSize &&
+            size.width > 0 &&
+            size.height > 0
+        ) {
+            val url = HttpUrl.parse(data)!!
+                .newBuilder()
+                .addQueryParameter("w", size.width.toString())
+                .addQueryParameter("h", size.height.toString())
+                .build()
+            val request = chain.request.newBuilder().data(url).build()
+            return chain.proceed(request)
+        }
+        return chain.proceed(chain.request)
+    }
+}
