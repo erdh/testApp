5c413ed766459e8bb9079af9e584c7ceaea21b6e
erdh <47626170+erdh@users.noreply.github.com>
2021-06-29 09:31:45 +0300

    splash activity'nin süresi azaltıldı (TODO bölümüne bakılmalı)

--- app/src/main/java/com/example/test/SplashScreen.kt
+++ app/src/main/java/com/example/test/SplashScreen.kt
@@ -25,6 +25,6 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 500) // 500 is the delayed time in milliseconds. //TODO: Delay olmadan bir çözüm bulmak
     }
 }
